72593eaffeb00eac9743bf4d3165b80372f099a5
knebadze <nebo.nebadze98@gmail.com>
2022-09-09 11:16:56 +0400

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -127,16 +127,16 @@
           </div>
         </form>
 
-        {{-- <div class="social-auth-links text-center mb-3">
-          <p>- OR -</p>
+        <div class="social-auth-links text-center mb-3">
+          {{-- <p>- OR -</p>
           <a href="#" class="btn btn-block btn-primary">
             <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
           </a>
           <a href="#" class="btn btn-block btn-danger">
             <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-          </a>
+          </a> --}}
         </div>
-        <!-- /.social-auth-links --> --}}
+        <!-- /.social-auth-links -->
 
         <p class="mb-1">
             @if (Route::has('password.request'))

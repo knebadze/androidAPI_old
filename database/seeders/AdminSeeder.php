d48fbfadd1ae9a5301c2468613e42b1b4bac84a9
knebadze <nebo.nebadze98@gmail.com>
2022-09-09 10:15:36 +0400

    first commit

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class AdminSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class AdminSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = User::creat([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        ]);
+        $user->assignRole('main', 'admin');
     }
 }

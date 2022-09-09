f1be2f6ac397549e9177c27f152b0a3f434a68c6
knebadze <nebo.nebadze98@gmail.com>
2022-09-09 10:27:52 +0400

    first commit

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -25,7 +25,7 @@ class AdminSeeder extends Seeder
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
 
         ]);
-        $user->assignRole('main', 'admin');
+        $user->assignRole('admin', 'admin');
 
     }
 }
